f0b23bc4e9f3322b902536a1ddfbbbd7e461abb9
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-02-04 08:20:30 +0200

    Add initial project files and configuration for Next.js with Tailwind CSS and NextAuth

---
+++ application/src/auth.config.ts
@@ -0,0 +1,16 @@
+import type { NextAuthConfig } from "next-auth"
+import Nodemailer from "next-auth/providers/nodemailer"
+ 
+export default { providers: [
+    Nodemailer({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    })
+] } satisfies NextAuthConfig
--- application/src/auth.ts
+++ application/src/auth.ts
@@ -1,19 +1,12 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/prisma"
-import Nodemailer from "next-auth/providers/nodemailer"
+import { PrismaClient } from "@prisma/client"
+import authConfig from "./auth.config"
  
-export const { handlers, auth, signIn, signOut } = NextAuth({
+const prisma = new PrismaClient()
+ 
+export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Nodemailer({
-    server: {
-      host: process.env.EMAIL_SERVER_HOST,
-      port: process.env.EMAIL_SERVER_PORT,
-      auth: {
-        user: process.env.EMAIL_SERVER_USER,
-        pass: process.env.EMAIL_SERVER_PASSWORD,
-      },
-    },
-    from: process.env.EMAIL_FROM,
-  }),],
+  session: { strategy: "jwt" },
+  ...authConfig,
 })
