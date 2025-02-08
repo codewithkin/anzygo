32cc8f811a03555c9761ba920d06c098c830dd21
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-02-08 12:01:58 +0200

    Add axios for API requests; introduce public user field in User model and implement user fetching endpoint

---
+++ application/src/app/api/community/users/route.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET (request: NextRequest) {
+    try {
+        // Get all public users
+        const users = await prisma.user.findMany({
+            where: {
+                public: true
+            }
+        })
+
+        // return the users to the frontend
+        return NextResponse.json(users);
+    } catch (e) {
+        console.log("An error occured while fetching users: ", e)
+    }
+}
--- application/src/app/chats/@modal/(.)new/page.tsx
+++ application/src/app/chats/@modal/(.)new/page.tsx
@@ -11,6 +11,9 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "@heroui/input";
+import { Search } from "lucide-react";
+import { Label } from "@/components/ui/label";
 
 export default function NewChatModal() {
   const router = useRouter();
@@ -30,12 +33,21 @@ export default function NewChatModal() {
     <Dialog onOpenChange={() => router.back()} open={true}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
+          <Input
+            startContent={<Search className="text-primary" size={20} strokeWidth={1} />}
+            className=""
+            classNames={{
+              input: "placeholder:font-regular"
+            }}
+            placeholder="Search for someone..."
+          />
         </DialogHeader>
+
+        <article className="flex flex-col gap-2">
+          <Label>Public Users</Label>
+          <article className="flex flex-col gap-2">
+          </article>
+        </article>
       </DialogContent>
     </Dialog>
   );
