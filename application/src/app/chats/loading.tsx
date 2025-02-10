af8190ac3239b92b3eda5d19b0ba77f131881ba2
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-02-10 09:42:09 +0200

    [ADD]: loading page

---
+++ application/src/app/chats/loading.tsx
@@ -0,0 +1,20 @@
+import { Loader2 } from "lucide-react"
+
+function Loading() {
+
+
+  return (
+    <section className="w-full h-full flex flex-col justify-center items-center gap-4">
+        {/* Loading spinner */}
+        <Loader2 className="animate-spin text-primary" size={50} />
+
+        {/* Loading text */}
+        <article className="flex flex-col gap-2 text-white text-center items-cenetr justify-center">
+            <h2 className="text-xl font-semibold">Loading chats...</h2>
+            <p className="text-sm">Please be patient, this will only take a minute</p>
+        </article>
+    </section>
+  )
+}
+
+export default Loading
