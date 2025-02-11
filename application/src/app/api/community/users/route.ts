bf6b9a7a34cb2c8be81eb9f4f437e7fbe97d1973
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-11 15:28:33 +0200

    REMOVE: Delete dummy user creation logic from GET API route and improve error response

--- application/src/app/api/community/users/route.ts
+++ application/src/app/api/community/users/route.ts
@@ -3,22 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
-    // Create dummy users
-    await prisma.user.create({
-      data: {
-        email: "kinzdj,df",
-        name: "Kin test",
-        id: "123",
-      },
-    });
-
-    await prisma.user.create({
-      data: {
-        email: ",df",
-        name: "Kin test2",
-      },
-    });
-
     // Get all public users
     const users = await prisma.user.findMany({
       where: {
@@ -26,11 +10,13 @@ export async function GET(request: NextRequest) {
       },
     });
 
-    console.log("Users: ", users);
-
     // return the users to the frontend
     return NextResponse.json(users);
   } catch (e) {
     console.log("An error occured while fetching users: ", e);
+
+    return NextResponse.json({
+      message: "An error occured"
+    })
   }
 }
